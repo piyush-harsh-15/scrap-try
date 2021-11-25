0adddb37bc3d552f44649ac2bc4f8f17aea67d02
Piyush Harsh <76912417+piyush-harsh-15@users.noreply.github.com>
2021-11-25 23:58:20 +0530

    commit

---
+++ googleS.py
@@ -0,0 +1,20 @@
+from selenium import webdriver
+
+PATH = "C:\Program Files (x86)\chromedriver.exe"
+driver = webdriver.Chrome(PATH)
+
+# Taking input from user
+search_string = input("Input the URL or string you want to search for:")
+
+# This is done to structure the string
+# into search url.(This can be ignored)
+search_string = search_string.replace(' ', '+')
+
+# Assigning the browser variable with chromedriver of Chrome.
+# Any other browser and its respective webdriver
+# like geckodriver for Mozilla Firefox can be used
+browser = webdriver.Chrome('chromedriver')
+
+for i in range(1):
+	matched_elements = browser.get("https://www.google.com/search?q=" +
+									search_string + "&start=" + str(i))
